67a41bf7e68535f00ccda5fe4b0a2be5ff8443df
Grzegorz Rakoczy <grakoczy@gmail.com>
2017-04-07 11:14:42 +0200

    New plugin version number

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@
 bl_info = {
     "name": "Thea Render",
     "author": "Grzegorz Rakoczy",
-    "version": (1,5,7,758,1447),
+    "version": (1,5,8,759,1455),
     "blender": (2, 7, 7),
     "location": "Render>Engine>Thea Render",
     "description": "Thea Render",
